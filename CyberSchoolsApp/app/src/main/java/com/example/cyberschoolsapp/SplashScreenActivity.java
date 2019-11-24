2de2b955ec2bc2917f5131004a29bbf41ce68569
TanzoTheGreatest <ralephatathanyani@gmail.com>
2019-11-24 22:13:26 +0200

    Official App files
    These are all the files for version1.0

---
+++ CyberSchoolsApp/app/src/main/java/com/example/cyberschoolsapp/SplashScreenActivity.java
@@ -0,0 +1,36 @@
+package com.example.cyberschoolsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+
+import gr.net.maroulis.library.EasySplashScreen;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        EasySplashScreen config = new EasySplashScreen(SplashScreenActivity.this)
+                .withFullScreen()
+                .withTargetActivity(MainActivity.class)
+                .withSplashTimeOut(5000)
+                .withBackgroundColor(Color.parseColor("#FFA500"))
+                .withHeaderText("CyberSchools Core App")
+                .withFooterText("Version 1.0")
+                .withBeforeLogoText("")
+                .withAfterLogoText("")
+                .withLogo(R.mipmap.ic_launcher_round);
+
+        config.getHeaderTextView().setTextColor(Color.WHITE);
+        config.getFooterTextView().setTextColor(Color.WHITE);
+        config.getBeforeLogoTextView().setTextColor(Color.WHITE);
+        config.getAfterLogoTextView().setTextColor(Color.WHITE);
+
+        View easySplashScreen = config.create();
+        setContentView(easySplashScreen);
+    }
+}
